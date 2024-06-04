8f81235a0cf310d1236fff4d188bbeedfd60f274
_Kerman <kermanx@qq.com>
2024-06-04 11:38:56 +0800

    first commit

---
+++ .vitepress/config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitepress'
+import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Reactive VSCode',
+  description: 'Develop VSCode extension with Vue Reactivity API',
+  base: '/reactive-vscode/',
+  lang: 'en-US',
+  themeConfig: {
+    nav: [
+      { text: 'Examples', link: '/examples/' },
+    ],
+
+    sidebar: {
+      '/examples': [
+        {
+          text: 'Examples',
+          items: [
+            { text: 'Index', link: '/examples/' },
+          ],
+        },
+      ],
+    },
+  },
+
+  markdown: {
+    codeTransformers: [
+      transformerTwoslash({
+        twoslashOptions: {
+          compilerOptions: {
+          }
+        }
+      }),
+    ],
+  },
+})
---
+++ index.md
@@ -0,0 +1,15 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: Reactive VSCode # '<span class="p1">Reactive</span> <span class="p2">VSCode</span>'
+  text: "Extension API"
+  tagline: |
+    Develop <span class="i-vscode-icons:file-type-vscode text-2xl"></span> <span class="text-vscode">Extension</span> with <span class="i-vscode-icons:file-type-vue text-2xl"></span> <span class="text-reactive">Composition</span> API
+  image: /logo.svg
+  actions:
+    - theme: alt
+      text: Examples
+      link: /examples/
+---
